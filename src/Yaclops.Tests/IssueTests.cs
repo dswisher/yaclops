7d8f858e0fadcb2a72cd5078a2a9696d268d0d36
Doug Swisher <doug.swisher@dowjones.com>
2015-03-04 10:24:00 -0600

    Add test

---
+++ src/Yaclops.Tests/IssueTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Yaclops.Tests
+{
+    [TestFixture]
+    public class IssueTests
+    {
+        // Tests to reproduce reported issues
+
+        [Test, Ignore("How does this related to global flags?")]
+        public void UnknownCommand()
+        {
+            var parser = new CommandLineParser(new[] { new LoadCommand() });
+            var result = (LoadCommand)parser.Parse(new[] { "--max-rows", "3", "load" });
+            result.MaxRows.ShouldBe(3);
+        }
+
+
+
+        #region Test Commands
+
+        private class LoadCommand : ISubCommand
+        {
+            [CommandLineOption(ShortName = "m")]
+            public int MaxRows { get; set; }
+
+            public void Execute()
+            {
+            }
+        }
+
+        #endregion
+    }
+}
