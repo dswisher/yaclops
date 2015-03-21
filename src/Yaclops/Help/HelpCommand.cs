93db851c1a507811c57772b44d1f1ee93589cf2c
Doug Swisher <big.swish@gmail.com>
2015-03-21 15:19:38 -0500

    Print synopsis

--- src/Yaclops/Help/HelpCommand.cs
+++ src/Yaclops/Help/HelpCommand.cs
@@ -26,6 +26,9 @@ namespace Yaclops.Help
         {
             IConsole console = new WrappedConsole();
 
+            // TODO - allow the name to be overridden in settings
+            string exeName = Assembly.GetEntryAssembly().GetName().Name.ToLower();
+
             if (Commands.Any())
             {
                 ISubCommand command;
@@ -39,16 +42,65 @@ namespace Yaclops.Help
                     return;
                 }
 
-                // TODO - print out command detail!
+                // TODO - how to get help on multi-word commands, like bisect? ('sample help bisect' should show *something*)
 
-                console.WriteLine("Help on {0} (or any command) is not yet implemented.", command.Name());
+                // Print the synopsis
+                var type = command.GetType();
+
+                console.StartWrap("usage: {0} {1}", exeName, command.Name());
+
+                // TODO - repeat global flags?
+
+                // Write out the named parameters
+                var namedParams = type.GetProperties()
+                    .Where(x => x.GetCustomAttributes(typeof(CommandLineOptionAttribute), true).Any());
+
+                foreach (var p in namedParams)
+                {
+                    var att = (CommandLineOptionAttribute)p.GetCustomAttributes(typeof(CommandLineOptionAttribute), true).First();
+                    List<string> opts = new List<string>();
+
+                    if (!string.IsNullOrEmpty(att.LongName))
+                    {
+                        opts.Add("--" + att.LongName);
+                    }
+                    else
+                    {
+                        opts.Add("--" + p.Name.Decamel('-'));                        
+                    }
+
+                    if (!string.IsNullOrEmpty(att.ShortName))
+                    {
+                        opts.Add("-" + att.ShortName);
+                    }
+
+                    console.Write(" [{0}]", string.Join(" | ", opts));
+                }
+
+                // Write out the positional parameters
+                var positional = type.GetProperties()
+                    .Where(x => x.GetCustomAttributes(typeof (CommandLineParameterAttribute), true).Any());
+
+                foreach (var p in positional)
+                {
+                    // TODO - for lists, indicate mmultiple
+                    console.Write(" [<{0}>]", p.Name.Decamel());
+                }
+
+                // Finish off the synopsis
+                console.EndWrap();
+                console.WriteLine();
+
+                console.WriteLine(command.Summary());
+
+                // TODO - long description
+                console.WriteLine(command.Description());
+
+                // TODO - option details
             }
             else
             {
-                // TODO - allow the name to be overridden in settings
-                string command = Assembly.GetEntryAssembly().GetName().Name.ToLower();
-
-                console.StartWrap("usage: {0}", command);
+                console.StartWrap("usage: {0}", exeName);
 
                 // TODO - write global flags
 
